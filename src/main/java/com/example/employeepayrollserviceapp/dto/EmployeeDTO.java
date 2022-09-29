f74264ea831eee2b448f2d1d8f4a3c479291c736
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-09-29 10:16:26 +0530

    ADDED: Using Lombok Library to generate Getter and Setter in DTO

--- src/main/java/com/example/employeepayrollserviceapp/dto/EmployeeDTO.java
+++ src/main/java/com/example/employeepayrollserviceapp/dto/EmployeeDTO.java
@@ -1,7 +1,14 @@
 package com.example.employeepayrollserviceapp.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+
+//It include getter and setter # no need to make getter and setter
+@Getter
+@Setter
 public class EmployeeDTO {
     private String firstName;
     private String lastName;
@@ -29,59 +36,4 @@ public class EmployeeDTO {
         this.notes = notes;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getProfilePic() {
-        return profilePic;
-    }
-
-    public void setProfilePic(String profilePic) {
-        this.profilePic = profilePic;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String department) {
-        this.address = department;
-    }
-
-    public Long getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Long salary) {
-        this.salary = salary;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public String getNotes() {
-        return notes;
-    }
-
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
 }
--- src/main/java/com/example/employeepayrollserviceapp/model/Employee.java
+++ src/main/java/com/example/employeepayrollserviceapp/model/Employee.java
@@ -1,12 +1,15 @@
 package com.example.employeepayrollserviceapp.model;
 
 import com.example.employeepayrollserviceapp.dto.EmployeeDTO;
+import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.time.LocalDate;
 
+//It include getter and setter # no need to make getter and setter
+@Data
 //It is used to create the tables in the database
 @Entity
 public class Employee {
@@ -63,67 +66,4 @@ public class Employee {
         this.notes = employeeDTO.getNotes();
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getProfilePic() {
-        return profilePic;
-    }
-
-    public void setProfilePic(String profilePic) {
-        this.profilePic = profilePic;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Long getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Long salary) {
-        this.salary = salary;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public String getNotes() {
-        return notes;
-    }
-
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
 }
