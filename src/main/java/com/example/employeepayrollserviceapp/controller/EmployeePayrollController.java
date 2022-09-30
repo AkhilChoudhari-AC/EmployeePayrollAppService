00b6d3db778b9bf9462a52a41f3e76d91c748e09
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-09-30 17:41:52 +0530

    ADDED: Add validation to the file name in dto package

--- src/main/java/com/example/employeepayrollserviceapp/controller/EmployeePayrollController.java
+++ src/main/java/com/example/employeepayrollserviceapp/controller/EmployeePayrollController.java
@@ -11,11 +11,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
 
 @RestController
+@RequestMapping("/employeepayrollservice")
 public class EmployeePayrollController {
     @Autowired
     IEmployeePayrollService service;
@@ -38,52 +40,94 @@ public class EmployeePayrollController {
 
     //UC2
     //Ability to display welcome message
-    @GetMapping("/employeePayrollService")
-    public ResponseEntity<String> getWelcome() {
-        return new ResponseEntity<String>(service.getWelcome(), HttpStatus.OK);
-    }
-
-    //Ability to save employee details to repository
-    @PostMapping("/employeePayrollService/create")
-    public ResponseEntity<Employee> saveDataToRepo(@RequestBody EmployeeDTO employee) {
-        return new ResponseEntity<Employee>(service.postDataToRepo(employee), HttpStatus.OK);
-    }
-
-    //Ability to get all employees' data by findAll() method
-    @GetMapping("/employeePayrollService/get")
-    public ResponseEntity<List<Employee>> getAllDataFromRepo() {
-        return new ResponseEntity<List<Employee>>(service.getAllData(), HttpStatus.OK);
-    }
-
-    //Ability to get employee data by id
-    @GetMapping("/employeePayrollService/get/{id}")
-    public ResponseEntity<Employee> getDataFromRepoById(@PathVariable Integer id) {
-        Optional<Employee> employee = service.getDataById(id);
-        ResponseDTO dto = new ResponseDTO("Data",employee);
-        return new ResponseEntity(dto, HttpStatus.OK);
-    }
-
-//    //ability to update employee data for particular id
-//    @PutMapping("/employeePayrollService/update/{id}")
-//    public ResponseEntity<Employee> updateDataInRepo(@PathVariable Integer id, @RequestBody Employee employee) {
-//        return new ResponseEntity<Employee>(service.updateDataById(id, employee), HttpStatus.OK);
+//    @GetMapping("/employeePayrollService")
+//    public ResponseEntity<String> getWelcome() {
+//        return new ResponseEntity<String>(service.getWelcome(), HttpStatus.OK);
 //    }
-
-    //UC3
+//
+//    //Ability to save employee details to repository
+//    @PostMapping("/employeePayrollService/create")
+//    public ResponseEntity<Employee> saveDataToRepo(@RequestBody EmployeeDTO employee) {
+//        return new ResponseEntity<Employee>(service.postDataToRepo(employee), HttpStatus.OK);
+//    }
+//
+//    //Ability to get all employees' data by findAll() method
+//    @GetMapping("/employeePayrollService/get")
+//    public ResponseEntity<List<Employee>> getAllDataFromRepo() {
+//        return new ResponseEntity<List<Employee>>(service.getAllData(), HttpStatus.OK);
+//    }
+//
+//    //Ability to get employee data by id
+//    @GetMapping("/employeePayrollService/get/{id}")
+//    public ResponseEntity<Employee> getDataFromRepoById(@PathVariable Integer id) {
+//        Optional<Employee> employee = service.getDataById(id);
+//        ResponseDTO dto = new ResponseDTO("Data",employee);
+//        return new ResponseEntity(dto, HttpStatus.OK);
+//    }
+//
+////    //ability to update employee data for particular id
+////    @PutMapping("/employeePayrollService/update/{id}")
+////    public ResponseEntity<Employee> updateDataInRepo(@PathVariable Integer id, @RequestBody Employee employee) {
+////        return new ResponseEntity<Employee>(service.updateDataById(id, employee), HttpStatus.OK);
+////    }
+//
+//    //UC3
+//    //Ability to update employee data for particular id
+//    @PutMapping("/employeePayrollService/update/{id}")
+//    public ResponseEntity<String> updateDataInRepo(@PathVariable Integer id, @RequestBody EmployeeDTO employeeDTO) {
+//        Employee employee = service.updateDataById(id, employeeDTO);
+//        ResponseDTO responseDTO = new ResponseDTO("Updating Employee PayrollData Successfuly:", employee);
+//        return new ResponseEntity(employeeDTO, HttpStatus.OK);
+//    }
+//
+//    //UC4: Introducing the service layer and implement the interface class
+//
+//    //Ability to delete employee data for particular id
+//    @DeleteMapping("/employeePayrollService/delete/{id}")
+//    public ResponseEntity<String> deleteDataInRepo(@PathVariable Integer id) {
+//        return new ResponseEntity<String>(service.deleteDataById(id), HttpStatus.OK);
+//    }
+//
+//}
+    // UC 5 AND SO ON
+    //Ability to display welcome message
+    @GetMapping("")
+    public ResponseEntity<String> getWelcome(){
+        String welcome = service.getWelcome();
+        return new ResponseEntity<String>(welcome,HttpStatus.OK);
+    }
+    //Ability to save employee data to repo
+    @PostMapping("/create")
+    public ResponseEntity<String> addDataToRepo(@Valid @RequestBody EmployeeDTO employeeDTO){
+        Employee newEmployee = service.postDataToRepo(employeeDTO);
+        ResponseDTO dto = new ResponseDTO("Record Added Succesfully",newEmployee);
+        return new ResponseEntity(dto,HttpStatus.CREATED);
+    }
+    //Ability to get all employees' data by findAll() method
+    @GetMapping("/get")
+    public ResponseEntity<String>getAllDataFromRepo(){
+        List<Employee> listOfEmployee = service.getAllData();
+        ResponseDTO dto = new ResponseDTO("Record Retrieved Successfully", listOfEmployee);
+        return new ResponseEntity(dto,HttpStatus.OK);
+    }
+    //Ability to get employee data by id
+    @GetMapping("/get/{id}")
+    public ResponseEntity<String> getDataFromRepoById(@PathVariable Integer id){
+        Optional<Employee> existingEmployee = service.getDataById(id);
+        ResponseDTO dto = new ResponseDTO("Record for given ID Retrieved Successfully", existingEmployee);
+        return new ResponseEntity(dto,HttpStatus.OK);
+    }
     //Ability to update employee data for particular id
-    @PutMapping("/employeePayrollService/update/{id}")
-    public ResponseEntity<String> updateDataInRepo(@PathVariable Integer id, @RequestBody EmployeeDTO employeeDTO) {
-        Employee employee = service.updateDataById(id, employeeDTO);
-        ResponseDTO responseDTO = new ResponseDTO("Updating Employee PayrollData Successfuly:", employee);
-        return new ResponseEntity(employeeDTO, HttpStatus.OK);
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> updateDataInRepo(@PathVariable Integer id,@Valid @RequestBody EmployeeDTO employeeDTO){
+        Employee updatedEmployee = service.updateDataById(id, employeeDTO);
+        ResponseDTO dto = new ResponseDTO("Record for particular ID Updated Successfully",updatedEmployee);
+        return new ResponseEntity(dto,HttpStatus.ACCEPTED);
     }
-
-    //UC4: Introducing the service layer and implement the interface class
-
     //Ability to delete employee data for particular id
-    @DeleteMapping("/employeePayrollService/delete/{id}")
-    public ResponseEntity<String> deleteDataInRepo(@PathVariable Integer id) {
-        return new ResponseEntity<String>(service.deleteDataById(id), HttpStatus.OK);
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteDataInRepo(@PathVariable Integer id){
+        ResponseDTO dto = new ResponseDTO("Record for particular ID Deleted Successfully", service.deleteDataById(id));
+        return new ResponseEntity(dto,HttpStatus.ACCEPTED);
     }
-
 }
--- src/main/java/com/example/employeepayrollserviceapp/dto/EmployeeDTO.java
+++ src/main/java/com/example/employeepayrollserviceapp/dto/EmployeeDTO.java
@@ -5,6 +5,10 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 
 
@@ -15,10 +19,18 @@ import java.time.LocalDate;
 
 
 public class EmployeeDTO {
+
+    @Pattern(regexp="^[a-zA-Z]{3,10}$",message="Please Enter correct first name")
     private String firstName;
+
+    @Pattern(regexp="^[a-zA-Z]{2,15}$",message="Please Enter correct last name")
     private String lastName;
+    @NotEmpty(message="Please Enter profilePic")
     private String profilePic;
+    @NotEmpty(message="Please enter the address")
     private String address;
+    @Min(value=1000,message="Salary should be more than 1000")
+    @Max(value=100000,message="Salary should be more than 100000")
     private Long salary ;
     private LocalDate date;
     private String notes;
