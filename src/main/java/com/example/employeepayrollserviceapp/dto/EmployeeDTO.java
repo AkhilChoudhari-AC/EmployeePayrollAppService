b90021a21414cb08385145dcaefdbb2c6d9a4cf2
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-10-02 15:26:15 +0530

    ADDED: Add validation to Gender,Date,Salary,Department

--- src/main/java/com/example/employeepayrollserviceapp/dto/EmployeeDTO.java
+++ src/main/java/com/example/employeepayrollserviceapp/dto/EmployeeDTO.java
@@ -1,15 +1,14 @@
 package com.example.employeepayrollserviceapp.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
+import java.util.Date;
 
 
 //It include getter and setter # no need to make getter and setter
@@ -30,16 +29,19 @@ public class EmployeeDTO {
     private String profilePic;
     @NotEmpty(message="Please enter the address")
     private String address;
+
     @Min(value=1000,message="Salary should be more than 1000")
     @Max(value=100000,message="Salary should be more than 100000")
     private Long salary ;
-    @NotEmpty(message="Please enter the date")
+
+   // @PastOrPresent(message = "PastOrPresent")
+   //@JsonFormat(pattern = "YYYY-MM-DD")
     private LocalDate date;
     @NotEmpty(message="Please enter the notes")
     private String notes;
-
+    @NotEmpty(message="Please select the gender (M , F)")
     private String gender;
-
+    @NotEmpty(message="Please select the department (ME, CE, IT)")
     private String department;
 
 
--- src/main/java/com/example/employeepayrollserviceapp/model/Employee.java
+++ src/main/java/com/example/employeepayrollserviceapp/model/Employee.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.time.LocalDate;
+import java.util.Date;
 
 //It include getter and setter # no need to make getter and setter
 @Data
